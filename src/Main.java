5f328b5529555a17b0b7f4e282b9ea54e8ebed2f
tayssobrt <gui.andrevieira@gmail.com>
2026-01-13 13:28:06 -0300

    Update: Instaciando dados do cliente

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,27 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.TemporalAccessor;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+
+
+        Scanner sc = new Scanner(System.in);
+
+        DateTimeFormatter ftm = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+
+        System.out.println("Digite os dados do cliente: ");
+        System.out.println("Nome: ");
+        String nome = sc.nextLine();
+        System.out.println("Email: ");
+        String email = sc.nextLine();
+        System.out.println("Data de nascimento (DD/MM/YYYY): ");
+        LocalDate nasc = LocalDate.parse(sc.nextLine(), ftm);
+        sc.nextLine();
+        System.out.println("Digite o estado do pedido: ");
+        String status = sc.nextLine();
+        Cliente cliente = new Cliente(nome, email, nasc);
 
     }
 
