2c6f1eee93f5b7fecfb7575589e2ae5a9b132a21
tayssobrt <gui.andrevieira@gmail.com>
2026-01-13 20:17:28 -0300

    Update: criado Section de criar produto

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,9 @@
+import enums.StatusDePedido;
+
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.time.temporal.TemporalAccessor;
+import java.util.Date;
 import java.util.Scanner;
 
 public class Main {
@@ -18,10 +21,33 @@ public class Main {
         String email = sc.nextLine();
         System.out.println("Data de nascimento (DD/MM/YYYY): ");
         LocalDate nasc = LocalDate.parse(sc.nextLine(), ftm);
-        sc.nextLine();
         System.out.println("Digite o estado do pedido: ");
-        String status = sc.nextLine();
+        StatusDePedido status = StatusDePedido.valueOf(sc.next());
         Cliente cliente = new Cliente(nome, email, nasc);
+
+        Pedido pedido = new Pedido(LocalDate.now(), status, cliente);
+
+        System.out.println("Quantos itens tem este pedido? ");
+        int n = sc.nextInt();
+        sc.nextLine();
+
+        for (int i = 0; i < n; i++) {
+            System.out.println("Digite os dados do #" + (i+1) + "item");
+            System.out.println("Nome: ");
+            String pNome = sc.nextLine();
+            System.out.println("Preço: ");
+            Double preco = sc.nextDouble();
+
+            Produto produto = new Produto(pNome, preco);
+
+            sc.nextLine();
+            System.out.println("Quantidade: ");
+            Integer quantidade = sc.nextInt();
+            sc.nextLine();
+
+            OrdemDePedido ordemDePedido = new OrdemDePedido(quantidade, preco, produto);
+
+        }
 
     }
 
