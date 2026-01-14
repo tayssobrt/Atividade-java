a9bbb97352d96b745d8883f4e0110a1931352112
tayssobrt <gui.andrevieira@gmail.com>
2026-01-13 21:02:33 -0300

    Update: Criado orcamento final da nota

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,7 @@
 import enums.StatusDePedido;
-
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
-import java.time.temporal.TemporalAccessor;
-import java.util.Date;
 import java.util.Scanner;
 
 public class Main {
@@ -13,6 +11,7 @@ public class Main {
         Scanner sc = new Scanner(System.in);
 
         DateTimeFormatter ftm = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        DateTimeFormatter ftm2 = DateTimeFormatter.ofPattern("dd/MM/yyyy' as 'HH:mm");
 
         System.out.println("Digite os dados do cliente: ");
         System.out.print("Nome: ");
@@ -21,22 +20,22 @@ public class Main {
         String email = sc.nextLine();
         System.out.print("Data de nascimento (DD/MM/YYYY): ");
         LocalDate nasc = LocalDate.parse(sc.nextLine(), ftm);
-        
+
         System.out.print("Digite o estado do pedido: ");
-        StatusDePedido status = StatusDePedido.valueOf(sc.next());
+        StatusDePedido status = StatusDePedido.valueOf(sc.next().toUpperCase());
         Cliente cliente = new Cliente(nome, email, nasc);
 
-        Pedido pedido = new Pedido(LocalDate.now(), status, cliente);
+        Pedido pedido = new Pedido(LocalDateTime.now(), status, cliente);
 
         System.out.print("Quantos itens tem este pedido? ");
         int n = sc.nextInt();
         sc.nextLine();
 
         for (int i = 0; i < n; i++) {
-            System.out.println("Digite os dados do #" + (i+1) + "item");
+            System.out.println("Digite os dados do #" + (i+1) + " item");
             System.out.print("Nome: ");
             String pNome = sc.nextLine();
-            System.out.println("Preço: ");
+            System.out.print("Preço: ");
             Double preco = sc.nextDouble();
 
             Produto produto = new Produto(pNome, preco);
@@ -47,9 +46,19 @@ public class Main {
             sc.nextLine();
 
             OrdemDePedido ordemDePedido = new OrdemDePedido(quantidade, preco, produto);
+            pedido.addItem(ordemDePedido);
 
         }
 
+        System.out.println("Orçamento: ");
+        System.out.println("horario: " + pedido.getHora().format(ftm2));
+        System.out.println("Status do pedido: " + pedido.getStatus().toString());
+        System.out.println("Cliente: " + cliente.getNome() + " " + cliente.getNasc().format(ftm) + " - " + cliente.getEmail());
+        System.out.println("Itens do pedido: ");
+        for (OrdemDePedido item : pedido.getOrdemDePedido()) {
+            System.out.println( item.getProduto().getNome() + ", Quantidade: " + item.getQuantidade() + ", R$" + item.subTotal());
+        }
+        System.out.println("TOTAL: " + pedido.total());
     }
 
 }
